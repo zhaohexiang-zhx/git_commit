48c08de63e348447a3ce174ff1a37388572d4af3
zhaohexiang <zhx190523@163.com>
2020-07-28 17:52:44 +0800

    base_url：1.设置baseurl 2.多环境切换

--- debugtalk.py
+++ debugtalk.py
@@ -3,3 +3,12 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def get_timestamp():
+    return str(int(time.time() * 10))
+
+def get_base_url(env_type="prod"):
+    if env_type == "prod":
+        return "http://10.23.190.107/zhgd_mms"  
+    else:
+        return "http://test.com/zhgd_mms"
